1ccfc84c96dbb10a8fb6a20236cbd02fe3c4a51e
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-27 00:09:26 +0100

    Updated configuration

---
+++ Infrastructure/HSINet.Identity.Infrastructure/AccessTokens/AccessTokenEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HSINet.Identity.Domain.AccessTokens;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HSINet.Identity.Infrastructure.AccessTokens;
+
+public class AccessTokenEntityConfiguration : IEntityTypeConfiguration<AccessToken>
+{
+    public void Configure(EntityTypeBuilder<AccessToken> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Id).HasField($"{nameof(AccessToken)}Id");
+    }
+}

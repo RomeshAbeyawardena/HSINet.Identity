0330c9f95b92875bfd3d9a1ffc2e79a52f9397f8
Romesh Abeyawardena <romesh.a@outlook.com>
2024-04-27 17:42:28 +0100

    Added ApiResponse

---
+++ Shared/HSINet.Identity.Http/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Text.Json.Serialization;
+
+namespace HSINet.Identity.Http;
+
+public class ApiResponse<T> : IStatusCodeActionResult
+{
+    public required T Data { get; init; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Error { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? Code { get; init; }
+
+    public DateTimeOffset RequestedTimeStampUtc { get; set; }
+    public int? StatusCode { get; }
+
+    public Task ExecuteResultAsync(ActionContext context)
+    {
+        var response = context.HttpContext.Response;
+        response.StatusCode = StatusCode.GetValueOrDefault(200);
+        return response.WriteAsJsonAsync(this);
+    }
+}
